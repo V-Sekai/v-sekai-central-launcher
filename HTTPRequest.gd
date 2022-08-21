760abf3cfc53b364cec44426b1fe94a19d485598
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-08-21 00:44:03 -0700

    Clean.

--- HTTPRequest.gd
+++ HTTPRequest.gd
@@ -1,39 +1,39 @@
 extends HTTPRequest
 
 
-func _ready():
-	var http_request = HTTPRequest.new()
+func _ready() -> void:
+	var http_request : HTTPRequest = HTTPRequest.new()
 	add_child(http_request)
 	http_request.request_completed.connect(self._http_request_completed)
-	var error = http_request.request("https://api.github.com/repos/V-sekai/mediapipe/releases")
+	var error : int = http_request.request("https://api.github.com/repos/V-sekai/mediapipe/releases")
 	if error != OK:
 		push_error("An error occurred in the HTTP request.")
-	connect("request_completed", Callable(self, "_on_request_completed"))
+	else:
+		connect("request_completed", Callable(self, "_on_request_completed"))
 
 
-func _http_request_completed(result, response_code, headers, body):
-	var json = JSON.new()
+func _http_request_completed(result, response_code, headers, body) -> void:
+	var json : JSON = JSON.new()
 	json.parse(body.get_string_from_utf8())
-	var response = json.get_data()
+	var response : Array = json.get_data()
 	if not response.size():
 		return
-	var elem_response = response[0]
+	var elem_response : Dictionary = response[0]
 	if not elem_response.has("assets"):
 		return
-	var assets = elem_response.assets
+	var assets : Array = elem_response.assets
 	if not assets.size():
 		return
-	var elem_asset = assets[0]
+	var elem_asset : Dictionary = assets[0]
 	if not elem_asset.has("browser_download_url"):
 		return
-	var url = elem_asset["browser_download_url"]
+	var url : String = elem_asset["browser_download_url"]
 	print(url)
 	request(url)
 
 
-func _on_request_completed(result, response_code, headers, body):
+func _on_request_completed(result, response_code, headers, body) -> void:
 	var buffer = body
-	var save = File.new()
+	var save : File = File.new()
 	save.open("exe.zip", File.WRITE)
 	save.store_buffer(buffer)
-
