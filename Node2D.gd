1f2beb0546f6c6a43e018e269832c7b254c8d6dc
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-08-21 00:12:52 -0700

    Remove unused code and set path to the self.

--- Node2D.gd
+++ Node2D.gd
@@ -1,12 +1,8 @@
 extends Control
 
-@export_file("*.exe") var wp_binary
 @export_global_dir var godot_project
 
 var pid = -1
-
-func _ready():
-	connect("tree_exiting", Callable(self, "_stop_wallpaper"))
 
 func _on_Button2_pressed():
 	var args = ["--path", godot_project]
